8ca4b3a531b7de904e39959cccfd39508c76c803
Ckandia <githaecarlos@gmail.com>
2026-09-14 17:10:32 +0300

    Add files via upload

--- types/index.ts
+++ types/index.ts
@@ -1,22 +1,5 @@
-import { TAccordionItem, TAccordionProps } from './accordion.types';
-import { TDatePickerOnChangeEvent, TGetCardLables, TGetContractTypeDisplay } from './common.types';
-import { TErrorMessages, TGetContractPath, TToastConfig } from './contract.types';
-import { TIconProps, TIconsManifest } from './icons.types';
-import { TMultiStepProps, TMultiStepRefProps } from './multi-step.types';
-import { TPopoverProps } from './popover.types';
-
-export type {
-    TAccordionItem,
-    TAccordionProps,
-    TDatePickerOnChangeEvent,
-    TErrorMessages,
-    TGetCardLables,
-    TGetContractPath,
-    TGetContractTypeDisplay,
-    TIconProps,
-    TIconsManifest,
-    TMultiStepProps,
-    TMultiStepRefProps,
-    TPopoverProps,
-    TToastConfig,
-};
+export * from './blockly.types';
+export * from './dbot.types';
+export * from './localize.types';
+export * from './strategy.types';
+export * from './ws.types';
