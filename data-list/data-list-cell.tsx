77eaf8b6319a94459b1b9474e7680a27f83d9e77
Ckandia <githaecarlos@gmail.com>
2026-09-14 17:46:12 +0300

    Add files via upload

--- data-list/data-list-cell.tsx
+++ data-list/data-list-cell.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { isTurbosContract, isVanillaContract } from '../shared';
+import { isTurbosContract, isVanillaContract } from '@/components/shared';
 import { TPassThrough, TRow } from '../types/common.types';
 
 export type TColIndex =
