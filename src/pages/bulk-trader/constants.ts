96a765857d81e80de514f79138ce1bf28148e870
Ckandia <githaecarlos@gmail.com>
2026-09-19 22:25:55 +0300

    Update constants.ts

--- src/pages/bulk-trader/constants.ts
+++ src/pages/bulk-trader/constants.ts
@@ -4,15 +4,23 @@ export const BULK_TRADER_MAX_STRATEGIES = 5;
 
 export const SYMBOL_OPTIONS: { value: string; label: string }[] = [
     { value: 'R_10', label: 'Volatility 10 Index' },
-    { value: 'R_25', label: 'Volatility 25 Index' },
-    { value: 'R_50', label: 'Volatility 50 Index' },
-    { value: 'R_75', label: 'Volatility 75 Index' },
-    { value: 'R_100', label: 'Volatility 100 Index' },
     { value: '1HZ10V', label: 'Volatility 10 (1s) Index' },
+    { value: '1HZ15V', label: 'Volatility 15 (1s) Index' },
+    { value: 'R_25', label: 'Volatility 25 Index' },
     { value: '1HZ25V', label: 'Volatility 25 (1s) Index' },
+    { value: '1HZ30V', label: 'Volatility 30 (1s) Index' },
+    { value: 'R_50', label: 'Volatility 50 Index' },
     { value: '1HZ50V', label: 'Volatility 50 (1s) Index' },
+    { value: 'R_75', label: 'Volatility 75 Index' },
     { value: '1HZ75V', label: 'Volatility 75 (1s) Index' },
+    { value: 'R_100', label: 'Volatility 100 Index' },
+    { value: '1HZ90V', label: 'Volatility 90 (1s) Index' },
     { value: '1HZ100V', label: 'Volatility 100 (1s) Index' },
+    { value: 'JD10', label: 'Jump 10 Index' },
+    { value: 'JD25', label: 'Jump 25 Index' },
+    { value: 'JD50', label: 'Jump 50 Index' },
+    { value: 'JD75', label: 'Jump 75 Index' },
+    { value: 'JD100', label: 'Jump 100 Index' },
 ];
 
 export const CONTRACT_TYPE_OPTIONS: { value: TDigitContractType; label: string; needs_prediction: boolean }[] = [
